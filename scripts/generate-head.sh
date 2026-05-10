2a0c03236f1cdab694ca4ab328b82acead3cc49a
André Gomes <andre34gomes@hotmail.com>
2026-05-10 21:37:51 +0100

    Fix Website Backend requests

--- scripts/generate-head.sh
+++ scripts/generate-head.sh
@@ -45,7 +45,7 @@ cat <<EOF
   <link rel="shortcut icon" href="${PREFIX}assets/favicon.svg">
   <link rel="mask-icon" href="${PREFIX}assets/favicon.svg" color="#D4AF37">
   <link rel="manifest" href="/manifest.json">
-  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
   <meta name="apple-mobile-web-app-title" content="Simphonia">
   <script src="${PREFIX}js/theme-init.js"></script>
